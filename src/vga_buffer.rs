0eb7942fb4b0ee7be3a6848599fbce9176ba6b75
mp3 <im.in.a.coma.xxx@gmail.com>
2021-05-04 12:10:38 +0900

    fix indentation

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -29,57 +29,58 @@ impl ColorCode {
     Color((background as u8) << 4 | (foreground as u8))
   }
 }
- #[derive(Debug, Clone, Copy, PartialEq, Eq)]
- #[repr(C)]
- struct ScreenChar {
-   ascii_character: u8,
-   color_code: ColorCode,
- }
 
- const BUFFER_HEIGHT: usize = 25;
- const BUFFER_WIDTH: usize = 80;
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+  ascii_character: u8,
+  color_code: ColorCode,
+}
 
- #[repr(transparent)]
- struct Buffer {
-   chars: [[ScreenChar; BUFFER_WIDTH; BUFFER_HEIGHT]]
- }
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
 
- pub struct Writer {
-   column_position: usize,
-   color_code: ColorCode,
-   buffer: &'static mut Buffer,
- }
+#[repr(transparent)]
+struct Buffer {
+  chars: [[ScreenChar; BUFFER_WIDTH; BUFFER_HEIGHT]]
+}
 
- impl Writer {
-   pub fn write_byte(&mut self, byte: u8) {
-     match byte {
-       b'/n' => self.new_line(),
-       byte => {
-         if self.column_position >= BUFFER_WIDTH {
-           self.new_line();
-         }
+pub struct Writer {
+  column_position: usize,
+  color_code: ColorCode,
+  buffer: &'static mut Buffer,
+}
 
-         let row = BUFFER_HEIGHT - 1;
-         let col = self.column_position;
+impl Writer {
+  pub fn write_byte(&mut self, byte: u8) {
+    match byte {
+      b'/n' => self.new_line(),
+      byte => {
+        if self.column_position >= BUFFER_WIDTH {
+          self.new_line();
+        }
 
-         let color_code = self.color_code;
-         self.buffer.chars[row][col] = ScreenChar {
-           ascii_character: byte,
-           color_code,
-         };
-         self.column_position += 1;
-       }
-     }
-   }
+        let row = BUFFER_HEIGHT - 1;
+        let col = self.column_position;
 
-   pub fn write_string(&mut self, s: &str) {
-     for byte in s.bytes() {
-       match byte {
-         0x20..=0x7e | b'\n' => self.write_byte(byte),
-         _ => self.write_byte(0xfe),
-       }
-     }
-   }
+        let color_code = self.color_code;
+        self.buffer.chars[row][col] = ScreenChar {
+          ascii_character: byte,
+          color_code,
+        };
+        self.column_position += 1;
+      }
+    }
+  }
 
-   fn new_line(&mut self) {/* TODO */}
- }
+  pub fn write_string(&mut self, s: &str) {
+    for byte in s.bytes() {
+      match byte {
+        0x20..=0x7e | b'\n' => self.write_byte(byte),
+        _ => self.write_byte(0xfe),
+      }
+    }
+  }
+
+  fn new_line(&mut self) {/* TODO */}
+}
