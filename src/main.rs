2f2107dfe97a8601a5a9e763fc060f64611388f9
mp3 <im.in.a.coma.xxx@gmail.com>
2021-05-16 18:08:28 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 use core::panic::PanicInfo;
 
@@ -32,15 +42,4 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
   println!("{}", info);
   loop {}
-}
-
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
 }
