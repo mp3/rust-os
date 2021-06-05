14271b2cb5e3d7266f34f1d05d63ed3770aac456
mp3 <im.in.a.coma.xxx@gmail.com>
2021-06-05 23:14:13 +0900

    impl Testable

--- src/main.rs
+++ src/main.rs
@@ -86,3 +86,14 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 pub trait Testable {
   fn run(&self) -> ();
 }
+
+impl<T> Testable for T
+where
+  T: Fn(),
+{
+  fn run(&self) {
+    serial_print!("{}...\t", core::any::type_name::<T>());
+    self();
+    serial_println("[ok]");
+  }
+}
