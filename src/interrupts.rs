1baca969dee5e7aad5e5edc007fea6a6801c25ec
mp3 <im.in.a.coma.xxx@gmail.com>
2021-06-19 22:08:27 +0900

    load idt

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -4,6 +4,7 @@ use crate::println;
 pub fn init_idt() {
   let mut idt = InterruptDescriptorTable::new();
   idt.breakpoint.set_handler_fn(breakpoint_handler);
+  idt.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
