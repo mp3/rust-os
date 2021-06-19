23761f6b765f65e3309aee38eb62ae38c29dc38c
mp3 <im.in.a.coma.xxx@gmail.com>
2021-06-19 22:03:48 +0900

    craete breakpoint_handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,13 @@
 pub mod interrupts;
 
-use x86_64::structures::idt::InterruptDescriptorTable;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use crate::println;
 
 pub fn init_idt() {
   let mut idt = InterruptDescriptorTable::new();
+  idt.breakpoint.set_handler_fn(breakpoint_handler);
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
+  println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
