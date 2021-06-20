a0fe6c9f9069b2caee07a9865272c90c2e3a44d0
mp3 <im.in.a.coma.xxx@gmail.com>
2021-06-20 17:10:18 +0900

    triggering a double fault

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,14 @@ pub extern "C" fn _start() -> ! {
 
   rusty_os::init();
 
-  x86_64::instructions::interrupts::int3();
+  unsafe {
+    *(0xdeadbeef as *mut u64) = 42;
+  }
 
   #[cfg(test)]
   test_main();
 
+  println!("It did not crash!");
   loop {}
 }
 
