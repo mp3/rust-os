b6ec6bf4b3b0c46fb52ab2f36c89a95756d5edbf
mp3 <im.in.a.coma.xxx@gmail.com>
2021-06-27 12:45:51 +0900

    test IDT

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -3,6 +3,25 @@
 
 use core::panic::PanicInfo;
 use rusty_os::serial_print;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::InterruptDescriptorTable;
+
+lazy_static! {
+  static ref TEST_IDT: InterruptDescriptorTable = {
+    let mut idt = InterruptDescriptorTable::new();
+    unsafe {
+      idt.double_fault
+        .set_handler_fn(test_double_fault_handler)
+        .set_stack_index(rusty_os::gdt::DOUBLE_FAULT_IST_INDEX);
+    }
+
+    idt
+  };
+}
+
+pub fn init_test_idt() {
+  TEST_IDT.load();
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
