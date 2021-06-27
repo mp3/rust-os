d7e856e5c6e2b89a7e575e0dc015aa3b040c0984
mp3 <im.in.a.coma.xxx@gmail.com>
2021-06-27 12:42:56 +0900

    implementing _start

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -2,10 +2,24 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use rusty_os::serial_print;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  unimplemented!();
+  serial_print!("stack_overflow::stack_overflow...\t");
+
+  rusty_os::gdt::init();
+  init_test_idt();
+
+  stack_overflow();
+
+  panic!("Execution continued after stack overflow");
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+  stack_overflow();
+  volatile::Volatile::new(0).read();
 }
 
 #[panic_handler]
