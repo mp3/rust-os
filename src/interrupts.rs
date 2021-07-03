7bacea58fbcf28ebef4efb9ca0fe50bbe5a7a5f4
mp3 <im.in.a.coma.xxx@gmail.com>
2021-07-03 22:11:39 +0900

    page fault

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -11,6 +11,8 @@ lazy_static! {
       idt.double_fault.set_handler_fn(double_fault_handler)
         .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
     }
+
+    idt.page_fault.set_handler_addr(page_fault_handler);
     idt
   };
 }
@@ -30,4 +32,20 @@ extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackF
 #[test_case]
 fn test_breakpoint_exception() {
   x86_64::instructions::interrupts::int3();
+}
+
+use x86_64::structures::idt::PageFaultErrorCode;
+use crate::hlt_loop;
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
